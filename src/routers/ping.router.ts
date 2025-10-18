203002089b9f0acaa34f76f8f512caad65f155db
Naman <goelnaman66@gmail.com>
2025-10-19 00:02:57 +0530

    middleware demo

--- src/routers/ping.router.ts
+++ src/routers/ping.router.ts
@@ -3,6 +3,17 @@ import { pingHandler } from '../controllers/ping.controller';
 
 const pingRouter = express.Router();
 
-pingRouter.get('/ping', pingHandler);
+
+function middleware1(req: express.Request, res: express.Response, next: express.NextFunction) {
+    console.log('Middleware 1 executed');
+    next();
+};
+
+function middleware2(req: express.Request, res: express.Response, next: express.NextFunction) {
+    console.log('Middleware 2 executed');
+    next();
+};
+
+pingRouter.get('/ping', middleware1, middleware2, pingHandler);
 
 export default pingRouter;
