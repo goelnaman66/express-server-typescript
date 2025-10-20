e58d3252fbeb37cddcfbc8de82808e1d92b03654
Naman <goelnaman66@gmail.com>
2025-10-20 09:45:53 +0530

    able to access query params but not body, whyt?

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 
 export const pingHandler =(req: Request, res: Response): void =>{
+    console.log(req.method, req.path);
+    console.log("Req Body", req.body);
+    console.log("Req Params", req.params);
+    console.log("Req Query params", req.query);
     res.send('pong');
 }
